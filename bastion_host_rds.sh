21799a92eee0f871e1cd94df42445bbc74bbc9f1
Christoph Gmeiner <christoph.gmeiner@gmail.com>
2023-03-23 15:27:57 +0100

    adapted direct bucket load

--- bastion_host_rds.sh
+++ bastion_host_rds.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -i bastion-host.pem -4 -N -L 5555:test-rds-cluster01.cluster-cfv4eklkdk8x.eu-central-1.rds.amazonaws.com:5432 ec2-user@52.29.90.136
+ssh -i "bastion-host.pem" -f -N -l ec2-user -L 5553:test-rds-cluster01.cluster-cfv4eklkdk8x.eu-central-1.rds.amazonaws.com:5432 18.198.6.174
